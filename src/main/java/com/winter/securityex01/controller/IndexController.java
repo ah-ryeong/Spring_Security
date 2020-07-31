1f4c2e21e70162f246d47ca551e9c0265a469819
unknown <aryeong22@hanail.net>
2020-07-31 14:40:43 +0900

    security - login

--- src/main/java/com/winter/securityex01/controller/IndexController.java
+++ src/main/java/com/winter/securityex01/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.winter.securityex01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.winter.securityex01.model.User;
+import com.winter.securityex01.repository.UserRepository;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping({"","/"})
 	public @ResponseBody String index() {
@@ -38,6 +47,10 @@ public class IndexController {
 	@PostMapping("/joinProc")
 	public String joinProc(User user) { // text나 JSON일때만 RequestBody 걸어준다.
 		System.out.println("회원가입 진행 :" + user);
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user);
 		return "redirect:/";
 	}
 }
